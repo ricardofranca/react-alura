b9e6f9ae856938ee1550df0f8dcf4e30999a1f50
Ricardo França <ricardodeoliveirafranca@gmail.com>
2017-06-08 19:23:34 -0300

    Finalizando aula 01

--- instalura/src/index.js
+++ instalura/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+//import registerServiceWorker from './registerServiceWorker';
+import './css/reset.css';
+import './css/timeline.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();

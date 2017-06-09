b10e29b81bae1e7c32f2f135b15e2243ef1bf703
Ricardo França <ricardodeoliveirafranca@gmail.com>
2017-06-09 10:51:00 -0300

    class 03 - adding react-router and login screen

---
+++ instalura/src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className="login-box">
+                <h1 className="header-logo">Instalura</h1>
+                <form>
+                    <input type="text"/>
+                    <input type="password"/>
+
+                    <input type="submit" value="Login"/>
+                </form>
+
+            </div>
+        );
+    }
+}
--- instalura/src/index.js
+++ instalura/src/index.js
@@ -4,7 +4,24 @@ import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 import './css/reset.css';
 import './css/timeline.css';
+import './css/login.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; //browserHistory
+import Login from './components/Login';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+    (
+        // can't destruct browserHistory from react-router-dom
+        // <Router history={browserHistory}>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Login} />
+                <Route path="/timeline" component={App} />
+            </Switch>
+        </Router>
+    )
+    ,
+    document.getElementById('root')
+);
 //registerServiceWorker();

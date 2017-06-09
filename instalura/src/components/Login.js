29c4e9412242f22bfa99166ea9eb10ab1c91f38e
Ricardo França <ricardodeoliveirafranca@gmail.com>
2017-06-09 11:34:40 -0300

    adding login functionality

--- instalura/src/components/Login.js
+++ instalura/src/components/Login.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 
+
 export default class Login extends Component {
+
+    constructor() {
+        super();
+
+        this.state = { msg: '' };
+        //this.envia = this.envia.bind(this); //can use like this or on the render as below
+    }
+
+    envia(event) {
+        event.preventDefault();
+        
+        const requestInfo = {
+            method: 'post',
+            body: JSON.stringify({ login: this.login.value, senha: this.senha.value }),
+            headers: new Headers({
+                'Content-type': 'application/json'
+            })
+        };
+
+        fetch('http://localhost:8080/api/public/login', requestInfo)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Não foi possível fazer login');
+                }
+
+                return response.text();
+            })
+            .then(token => { 
+                console.log(token); 
+                //browserHistory.push();
+            })
+            .catch(mensagem =>  { this.setState({ msg: 'Não foi possível fazer login' }) });
+    }
+
     render() {
         return (
             <div className="login-box">
                 <h1 className="header-logo">Instalura</h1>
-                <form>
-                    <input type="text"/>
-                    <input type="password"/>
+                <span>{this.state.msg}</span>
+                <form onSubmit={this.envia.bind(this)}>
+                    <input type="text" ref={(input) => this.login = input} />
+                    <input type="password" ref={(input) => this.senha = input} />
 
-                    <input type="submit" value="Login"/>
+                    <input type="submit" value="Login" />
                 </form>
 
             </div>

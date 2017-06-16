fd3ee1c818658ad818b99d51c881fc691bf75c49
Ricardo França <ricardodeoliveirafranca@gmail.com>
2017-06-15 22:41:11 -0300

    adding transtition to the photos

--- instalura/src/components/Timeline.js
+++ instalura/src/components/Timeline.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FotoItem from './FotoItem';
 import Pubsub from 'pubsub-js';
+import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
 
 export default class Timeline extends Component {
 
@@ -57,9 +58,14 @@ export default class Timeline extends Component {
     render() {
         return (
             <div className="fotos container">
-                {
-                    this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} />)
-                }
+                <ReactCSSTransitionGroup
+                    transitionName="timeline"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={300}>
+                    {
+                        this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} />)
+                    }
+                </ReactCSSTransitionGroup>
             </div>
         );
     }
